799816534877360e8473f8187a66dfa06d399c4b
Thành Zanh <114275350+thanhzanh@users.noreply.github.com>
2025-03-12 00:02:20 +0700

    Dang nhap bang Google

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,35 +1,61 @@
 import React, { useState } from "react";
 import banner from "../../assets/banner.png";
 import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode"; // Giải mã token Google
 import { useNavigate } from "react-router-dom";
 import { loginAccount } from "../../services/AccountService";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { toast } from "react-toastify";
+import axios from "axios";
 
 const Login = () => {
   const navigate = useNavigate();
 
-  const handleGoogleSuccess = () => {
-    window.open("http://localhost:3000/api/v1/users/auth/google", "_self");
-  };
+  const [errors, setErrors] = useState({});
 
-  const handleGoogleFailure = () => {
-    console.log("Login Failed");
-  };
-  const handleGoogleLogin = () => {
-    window.open("http://localhost:3000/api/v1/users/auth/google", "_self");
-  };
+  // Xử lý đăng nhập bằng Google
+  const handleGoogleSuccess = async (credentialResponse) => {
+    const token = credentialResponse.credential;
+    const userInfo = jwtDecode(token); // Giải mã để xem thông tin user
+    
+    try {
+      // Cách 1 dùng fetch
+      // const response = await fetch("http://localhost:3000/api/v1/users/auth/google", {
+      //   method: "POST",
+      //   headers: { "Content-Type": "application/json" },
+      //   body: JSON.stringify({ token })
+      // });
 
-  // login email & password
+      // Cách 2 dùng axios
+      const response = await axios.post("http://localhost:3000/api/v1/users/auth/google",
+        { token },
+        { withCredentials: true } // Dùng để gửi và nhận cookie
+      );
+
+      const data = response.data;      
+
+      if (data.success) {
+        toast.success("Đăng nhập thành công");
+
+        navigate("/workspace/exams/list");
+      } else {
+        toast.error("Đăng nhập Google thất bại");
+      }
+    } catch (error) {
+      console.error("Lỗi đăng nhập Google", error);
+      toast.error("Đã xãy ra lỗi");
+    }
+    
+  }
+
+  // Xử lý đăng nhập bằng email & password
   const [login, setLogin] = useState({
     email: "",
     password: "",
   });
 
   const [showPassword, setShowPassword] = useState(false);
-
-  const [errors, setErrors] = useState({});
 
   const handleChange = (e) => {
     setLogin({ ...login, [e.target.name]: e.target.value });
@@ -67,6 +93,7 @@ const Login = () => {
       }
     }
   };
+
   return (
     <div className="position-relative d-flex justify-content-center align-items-center vh-100">
       <img
@@ -83,7 +110,7 @@ const Login = () => {
 
         <GoogleLogin
           onSuccess={handleGoogleSuccess}
-          onError={handleGoogleFailure}
+          onError={() => {toast.error("Đăng nhập Google thất bại")}}
         />
         <p className="text-center mt-3 mb-3 or-divider">hoặc tiếp tục với</p>
         <form method="POST" onSubmit={handleLogin}>
